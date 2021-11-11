a057069880483cc75e845883dffb89a41a33e40b
Anja Skrlj <skrljanja@gmail.com>
2021-11-11 16:40:31 +0000

    publisher and subscriber added to control.py

--- src/control.py
+++ src/control.py
@@ -2,13 +2,19 @@
 
 import rospy
 import numpy as np
-from std_msgs.msg import Float64MultiArray
+from std_msgs.msg import Float64
 
 class control:
     
     def __init__(self): 
-        self.detect_joint_image = rospy.Subscriber('/robot/joint_states', Float64MultiArray, self.callback)
-    
+        self.joint1 = rospy.Subscriber('/joint_angle_1', Float64, self.callback)
+        self.joint3 = rospy.Subscriber('/joint_angle_3', Float64, self.callback)
+        self.joint4 = rospy.Subscriber('/joint_angle_4', Float64, self.callback)
+
+        self.joint1_control = rospy.Publisher('/robot/joint1_position_controller/command', Float64, queue_size = 10)
+        self.joint1_control = rospy.Publisher('/robot/joint1_position_controller/command', Float64, queue_size = 10)
+        self.joint1_control = rospy.Publisher('/robot/joint1_position_controller/command', Float64, queue_size = 10)
+
     def forward_kinematics(self,image):
         joints = self.detect_joint_angles(image)
         end_effector_x = (2.8 * ( - np.sin(joints[0])*np.cos(joints[2])*np.sin(joints[3]) 
