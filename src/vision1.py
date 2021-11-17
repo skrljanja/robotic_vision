6c3c1fce4882d400767de7c2f9a31bd6d950c148
Yanchick01 <Yanegubko01@gmail.com>
2021-11-17 23:35:46 +0000

    adding the code from image files to vision1 file

--- src/vision1.py
+++ src/vision1.py
@@ -2,7 +2,16 @@
 
 import rospy
 from sensor_msgs.msg import Image
-from std_msgs.msg import Float64
+#from std_msgs.msg import Float64
+
+# imports from image files
+import roslib
+import sys
+import cv2
+import numpy as np
+from std_msgs.msg import String
+from std_msgs.msg import Float64MultiArray, Float64
+from cv_bridge import CvBridge, CvBridgeError
 
 
 class vision1: 
@@ -10,8 +19,8 @@ class vision1:
     def __init__(self):
     
     	# defining subscribers
-        self.image1_sub = rospy.Subscriber('camera1/robot/image_raw', Image, self.callback)
-        self.image2_sub = rospy.Subscriber('camera2/robot/image_raw', Image, self.callback)
+        self.image1_sub = rospy.Subscriber('camera1/robot/image_raw', Image, self.callback1)
+        self.image2_sub = rospy.Subscriber('camera2/robot/image_raw', Image, self.callback2)
         
         # defining publishers  (btw are you sure we define each joint separately and not like in lab 1? "self.joints_pub = rospy.Publisher("joints_pos",Float64MultiArray, queue_size=10)")
         self.joint2 = rospy.Publisher('/joint_angle_2', Float64, queue_size = 10)
@@ -93,3 +102,78 @@ class vision1:
       ja2 = np.arctan2(circle1Pos[0]-circle2Pos[0], circle1Pos[1]-circle2Pos[1]) - ja1
       ja3 = np.arctan2(circle2Pos[0]-circle3Pos[0], circle2Pos[1]-circle3Pos[1]) - ja2 - ja1
       return np.array([ja1, ja2, ja3])
+  
+  
+# code from image files    
+class image_converter:
+
+  # Defines publisher and subscriber
+  def __init__(self):
+    # initialize the node named image_processing
+    rospy.init_node('image_processing', anonymous=True)
+    
+    # image1
+    # initialize a publisher to send images from camera1 to a topic named image_topic1
+    self.image_pub1 = rospy.Publisher("image_topic1",Image, queue_size = 1)
+    # initialize a subscriber to recieve messages rom a topic named /robot/camera1/image_raw and use callback function to recieve data
+    self.image_sub1 = rospy.Subscriber("/camera1/robot/image_raw",Image,self.callback1)
+    
+    # image2
+    # initialize a publisher to send images from camera2 to a topic named image_topic2
+    self.image_pub2 = rospy.Publisher("image_topic2",Image, queue_size = 1)
+    # initialize a subscriber to recieve messages rom a topic named /robot/camera1/image_raw and use callback function to recieve data
+    self.image_sub2 = rospy.Subscriber("/camera2/robot/image_raw",Image,self.callback2)
+    
+    # initialize the bridge between openCV and ROS
+    self.bridge = CvBridge()
+
+
+  # Recieve data from camera 1, process it, and publish
+  def callback1(self,data):
+    # Recieve the image
+    try:
+      self.cv_image1 = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+    
+    # Uncomment if you want to save the image
+    #cv2.imwrite('image_copy.png', cv_image)
+
+    im1=cv2.imshow('window1', self.cv_image1)
+    cv2.waitKey(1)
+    # Publish the results
+    try: 
+      self.image_pub1.publish(self.bridge.cv2_to_imgmsg(self.cv_image1, "bgr8"))
+    except CvBridgeError as e:
+      print(e)
+      
+  # Recieve data from camera 2, process it, and publish
+  def callback2(self,data):
+    # Recieve the image
+    try:
+      self.cv_image2 = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+    # Uncomment if you want to save the image
+    #cv2.imwrite('image_copy.png', cv_image)
+    im2=cv2.imshow('window2', self.cv_image2)
+    cv2.waitKey(1)
+
+    # Publish the results
+    try: 
+      self.image_pub2.publish(self.bridge.cv2_to_imgmsg(self.cv_image2, "bgr8"))
+    except CvBridgeError as e:
+      print(e)
+
+# call the class
+def main(args):
+  ic = image_converter()
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+# run the code if the node is called
+if __name__ == '__main__':
+    main(sys.argv)
